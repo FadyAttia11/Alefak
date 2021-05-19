888767ce97a6361056997399edcc4bf33d4273e3
Fady Attia <fady.attia@callvita-eg.com>
2021-05-19 21:12:16 +0200

    fix: added logout to index admin

--- index-admin.php
+++ index-admin.php
@@ -55,24 +55,8 @@ session_start();
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#team">Team</a></li>
           <li><a href="blog.html">Blog</a></li>
-          <li class="drop-down"><a href="">Drop Down</a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="drop-down"><a href="#">Deep Drop Down</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#">Balance: <?php echo $user_data['balance'] ?> L.E</a></li>
+          <li><a href="logout.php">Logout</a></li>
 
         </ul>
       </nav><!-- .nav-menu -->
