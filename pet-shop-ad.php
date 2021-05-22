21301d0014cb767e0c532471e690a4ab1169cf50
Fady Attia <fadyattia11@gmail.com>
2021-05-22 17:41:27 +0200

    finished pet shop for admin

---
+++ pet-shop-ad.php
@@ -0,0 +1,188 @@
+<?php
+session_start();
+
+    include("connection.php");
+    include("functions.php");
+
+    $user_data = check_login($con);
+
+    $all_store_query = "select * from store";
+    $all_store = mysqli_query($con, $all_store_query);
+
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+        if(isset($_POST['all'])) {
+          $all_store_query = "select * from store";
+          $all_store = mysqli_query($con, $all_store_query);
+  
+        } else if(isset($_POST['food'])) {
+          $all_store_query = "select * from store where category = 'Food'";
+          $all_store = mysqli_query($con, $all_store_query);
+  
+        } else if(isset($_POST['accessories'])) {
+          $all_store_query = "select * from store where category = 'Accessories'";
+          $all_store = mysqli_query($con, $all_store_query);
+
+        }else if(isset($_POST['item_id'])) {
+            $item_id = $_POST['item_id'];
+
+            $delete_item_query = "delete from store where id = '$item_id'";
+            $delete_item = mysqli_query($con, $delete_item_query);
+
+            if($delete_item) {
+                echo "successfully deleted your item";
+                header('Location: pet-shop-ad.php');
+            }
+        }
+        
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Alefak</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top header-inner-pages">
+    <div class="container d-flex align-items-center">
+
+      <!-- <h1 class="logo mr-auto"><a href="index.php">Alefak</a></h1> -->
+      <a href="index.php" class="logo mr-auto"><img src="assets/img/logo.jpg" alt="" class="img-fluid"></a>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="new-shop.php">Add To Shop</a></li>
+          <li class="active"><a href="pet-shop-ad.php">Pet Shop</a></li>
+          <li><a href="shop-reserv.php">Shop Reservations</a></li>
+          <li><a href="vet-reserv.php">Vet Reservations</a></li>          
+          <li><a href="#">Balance: <?php echo $user_data['balance'] ?> L.E</a></li>
+          <li><a href="logout.php">Logout</a></li>
+
+        </ul>   
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Pet Shop</li>
+        </ol>
+        <h2>Pet Shop</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Portfolio Details Section ======= -->
+<section id="portfolio-details" class="portfolio-details">
+<div class="container">
+
+    <div class="row">
+        <div class="col-lg-3">
+            <h1 class="my-4">Categories</h1>
+            <div class="list-group">
+                <form method="post">
+                    <input type="hidden" name="all" value="all">
+                    <input type="submit" class="list-group-item" value="All Store">
+                </form>
+
+                <form method="post">
+                <input type="hidden" name="food" value="food">
+                <input type="submit" class="list-group-item" value="Food">
+                </form>
+
+                <form method="post">
+                <input type="hidden" name="accessories" value="accessories">
+                <input type="submit" class="list-group-item" value="Accessories">
+                </form>
+            </div>
+        </div>
+
+        <div class="col-lg-9">
+            <div class="row">
+                <?php
+                while($row = mysqli_fetch_array($all_store)) {
+                ?>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src=<?php echo './uploads/'. $row['image'] ?> alt=""></a>
+                    <div class="card-body">
+                    <h4 class="card-title">
+                        <a href="#"><?php echo $row['item_name'] ?></a>
+                    </h4>
+                    <h5><?php echo $row['price'] ?> L.E</h5>
+                    <p class="card-text"><?php echo $row['description'] ?></p>
+                    </div>
+                    <div class="card-footer">
+                    <form method="post">
+                        <input type="hidden" name="item_id" value=<?php echo $row['id'] ?>>
+                        <input type="submit" class="btn btn-danger" value="Delete This Item">
+                    </form>
+                    </div>
+
+                </div>
+                </div>
+
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+
+</div>
+</section>
+</main>
+
+
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
